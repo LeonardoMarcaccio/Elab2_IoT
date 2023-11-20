3863faec85b0ce99ced02ba875111118d1bb4237
Marco Francolini <marco.francolini@studio.unibo.it>
2023-11-20 19:51:21 +0100

    Restructured entire project and commented code that used an obsolete library

--- Carwash/src/LCDImpl.cpp
+++ Carwash/src/LCDImpl.cpp
@@ -4,8 +4,8 @@
 #include <WString.h>
 
 #include "Wire.h"
-#include "LCD.h"
-#include "LiquidCrystal_I2C.h"
+//#include "LCD.h"
+//#include "LiquidCrystal_I2C.h"
 
 using namespace std;
 
@@ -17,15 +17,15 @@ class LCDImpl : public SimpleLCD {
         int PIN_2;
     public :
         LCDImpl(String t, int pin_1, int pin_2) : text(t), PIN_1(pin_1), PIN_2(pin_2) {
-            LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
-            lcd.begin(16,2);
-            lcd.setBacklightPin(3,POSITIVE);
-            lcd.setBacklight(HIGH);
+//            LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
+//            lcd.begin(16,2);
+//            lcd.setBacklightPin(3,POSITIVE);
+//            lcd.setBacklight(HIGH);
         }
 
         void setDisplayText(String text) {
             this->text = text;
-            lcd.print(this->text);
+//            lcd.print(this->text);
         };
 
         String getDisplayText() {
