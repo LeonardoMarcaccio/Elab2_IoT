b371a88167ba455077b429a76329e5f9737802a1
Lorenzo Dalmonte - lorenzo.dalmonte4@studio.unibo.it <lorenzo.dalmonte4@studio.unibo.it>
2023-11-22 10:30:24 +0100

    Updated LCDImpl.cpp

--- Carwash/src/components/LCDImpl.cpp
+++ Carwash/src/components/LCDImpl.cpp
@@ -4,8 +4,7 @@
 #include <WString.h>
 
 #include "Wire.h"
-//#include "LCD.h"
-//#include "LiquidCrystal_I2C.h"
+#include <LiquidCrystal_I2C.h>
 
 using namespace std;
 
@@ -13,19 +12,17 @@ class LCDImpl : public SimpleLCD {
 
     private :
         String text;
-        int PIN_1;
-        int PIN_2;
+        LiquidCrystal_I2C lcd = lcd(0x27, 16, 2);
     public :
-        LCDImpl(String t, int pin_1, int pin_2) : text(t), PIN_1(pin_1), PIN_2(pin_2) {
-//            LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
-//            lcd.begin(16,2);
-//            lcd.setBacklightPin(3,POSITIVE);
-//            lcd.setBacklight(HIGH);
+        LCDImpl(String t) : text(t) {
+            lcd.init();
+            lcd.backlight();
         }
 
         void setDisplayText(String text) {
             this->text = text;
-//            lcd.print(this->text);
+            lcd.setCursor(0, 0);
+            lcd.print(this->text);
         };
 
         String getDisplayText() {
@@ -33,6 +30,6 @@ class LCDImpl : public SimpleLCD {
         };
 
         void clear() {
-            this->setDisplayText("");
+            lcd.clear();
         };
 };
