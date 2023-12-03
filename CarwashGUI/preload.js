234409a9e50a482a093f02ff1b72b1a168563145
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2023-12-03 11:33:20 +0100

    Added serial port support for GUI

--- CarwashGUI/preload.js
+++ CarwashGUI/preload.js
@@ -1,6 +1,24 @@
 const { contextBridge, ipcRenderer } = require('electron')
-const SerialPort = require('serialport');
+//const SerialPort = require('serialport');
+//const Readline = require('@serialport/parser-readline');
 const fs = require('fs')
+// TODO: Fix this mess
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+//const { path } = require('path')
+//var serialPortList = await SerialPort.list()
+SerialPort.list().then(result => console.log(result))
+const port = new SerialPort({
+	path:'COM5',
+	baudRate:9600
+})
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+port.on("open", () => {
+  console.log('serial port open');
+});
+parser.on('data', data =>{
+  console.log('got word from arduino:', data);
+});
 
 /**
  * Checks if a string starts with any of the strings passed.
@@ -40,12 +58,17 @@ const appInteraction = {
 	interProcessRenderer: ipcRenderer
 }
 
-const hwInteraction = {
-	SerialPort: SerialPort
+const libraryInteraction = {
+	safeRequire: (value) => {
+		const allowed = ["serialport", "@serialport/parser-readline"]
+		if (allowed.includes(value)) {
+			return require(value)
+		}
+	}
 }
 
 contextBridge.exposeInMainWorld("internalApis", {
 	fsInteraction: fsInteraction,
 	appInteraction: appInteraction,
-	hwInteraction: hwInteraction
+	libraryInteraction: libraryInteraction
 })
--- CarwashGUI/renderer.js
+++ CarwashGUI/renderer.js
@@ -1,3 +1,6 @@
+//const SerialPort = window.internalApis.libraryInteraction.safeRequire('serialport');
+//const Readline = window.internalApis.libraryInteraction.safeRequire('@serialport/parser-readline');
+
 document.body.onload = () => initGUI()
 
 function initTopBarArea() {
@@ -19,3 +22,15 @@ function initGUI() {
     initBaseElements()
     generateView()
 }
+/*//const SerialPort = window.internalApis.hwInteraction.SerialPort
+//const Readline = window.internalApis.hwInteraction.SerialReadLine
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const parser = port.pipe(new SerialReadLine({ delimiter: '\n' }));
+// Read the port data
+port.on("open", () => {
+  console.log('serial port open');
+});
+parser.on('data', data =>{
+  console.log('got word from arduino:', data);
+});
+*/
